f100c66117d633618e69de1097cd28c7eb7b1918
CookieJarCoding <wigginhigginliggintiggins@gmail.com>
2026-09-16 22:31:30 +0800

    Text scrolling blip sounds.
    BUG?: Short delay when playing a sound for the first time. Could be due to slow initialization.

--- dialogue_system/scripts/dialogue_hud.gd
+++ dialogue_system/scripts/dialogue_hud.gd
@@ -1,26 +1,37 @@
+'''
+Tutorial from https://worldeater-dev.itch.io/bittersweet-birthday/devlog/224241/howto-a-simple-dialogue-system-in-godot. I deviated at times for our purposes.
+'''
+
+
 extends CanvasLayer
 class_name Dialogue
 
-'''
-Tutorial from https://worldeater-dev.itch.io/bittersweet-birthday/devlog/224241/howto-a-simple-dialogue-system-in-godot
-'''
 
 @onready var dialogue_label: RichTextLabel = $MainContainer/ContentContainer/DialogueLabel
 # NOTE: Currently unused
 @onready var pause_timer: Timer = $PauseTimer
+@onready var blip: AudioStreamPlayer = $DialogueBlip
 
 
 var timer: float = 0
 var is_timer_running: bool = false
 # NOTE: Letters rendered per second.
-var message_speed: float = 10.0
+var message_speed: float = 30.0
+var rng = RandomNumberGenerator.new()
 
+
+'''
+Main functions
+'''
 
 func _ready() -> void:
-	update_message("Hello! This is a piece of dialogue.")
+	pass
 
 
 func _process(delta: float) -> void:
+	if OS.is_debug_build() and Input.is_action_just_pressed("debug_m"):
+		update_message("Hello! This is a piece of dialogue.")
+	
 	if timer > (1.0 / message_speed):
 		display_next_character()
 		timer = 0
@@ -35,10 +46,24 @@ func update_message(message: String) -> void:
 	start_timer()
 
 
-func start_timer() -> void:
-	is_timer_running = true
-
-
 func display_next_character() -> void:
 	if dialogue_label.visible_characters < dialogue_label.text.length():
 		dialogue_label.visible_characters += 1
+		# NOTE: Using this implementation, the MINIMUM blip length is set by
+		# the length of the .wav file.
+		if not blip.playing:
+			blip.pitch_scale = rng.randf_range(0.95, 1.08)
+			
+			# WARNING/TODO: There seems to be a short delay when playing an AudioStream
+			# for the first time. To be fixed
+			blip.play(0)
+
+
+
+'''
+Helper functions
+'''
+
+
+func start_timer() -> void:
+	is_timer_running = true
