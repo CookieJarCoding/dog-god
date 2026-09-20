afa29055f3042b3b325c62c9ed5b6b92f0d59d16
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-09-20 23:20:33 +0800

    refactor: prefix unused body variables

--- sidescroll_system/rooms/kitchen/phase0/kitchen_phase_0.gd
+++ sidescroll_system/rooms/kitchen/phase0/kitchen_phase_0.gd
@@ -30,7 +30,7 @@ func _ready() -> void:
 	])
 
 
-func _on_dialogue_trigger_area_body_entered(body: Node2D) -> void:
+func _on_dialogue_trigger_area_body_entered(_body: Node2D) -> void:
 	if _is_dialogue_finished:
 		return
 	
