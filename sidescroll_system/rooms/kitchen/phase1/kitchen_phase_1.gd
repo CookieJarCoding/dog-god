c76d0da35cb2ed24552b8882d123796e0166e226
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-09-20 23:47:59 +0800

    feat: complete kitchen phase 1

---
+++ sidescroll_system/rooms/kitchen/phase1/kitchen_phase_1.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+var _dialogue_manager: DialogueManager
+
+func _ready() -> void:
+	_dialogue_manager = DialogueManager.get_instance(get_tree())
+	if _dialogue_manager == null:
+		push_error("No DialogueManager defined for this room.")
+		return
+
+	_dialogue_manager.load_messages([
+		"[color=#1f1f1f]My, my, Brioche![/color]",
+		"[color=#1f1f1f]You basically inhaled your food! Hehe.[/color]",
+		"[color=#1f1f1f]I guess all that running around earlier[/color]",
+		"[color=#1f1f1f]worked up your appetite.[/color]",
+		"[color=#1f1f1f]Brioche aggressively bites the dog bowl as if[/color]",
+		"[color=#1f1f1f]to eat it.[/color]",
+		"[color=#1f1f1f]Hey, hey, no. The bowl stays. You can't eat that.[/color]",
+		"[color=#1f1f1f]Pita chuckles.[/color]",
+		"[color=#1f1f1f]Brioche rolls her eyes.[/color]",
+		"[color=#1f1f1f]Boof![/color]",
+		"[color=#1f1f1f]Pita yawns.[/color]",
+		"[color=#1f1f1f]C'mon boy, let's hit the sack, whaddouyou think?[/color]",
+		"[color=#1f1f1f]Boof.[/color]",
+		"[color=#1f1f1f](Alas, I could not consume more of this)[/color]",
+		"[color=#1f1f1f](portal. Nevertheless, it is still activated.)[/color]",
+		"[color=#1f1f1f](Not as strong as I'd hope, but it[/color]",
+		"[color=#1f1f1f]shall suffice.)[/color]",
+		"[color=#1f1f1f](The human demands sleepy time.)[/color]",
+		"[color=#1f1f1f](So sleepy time I shall.)[/color]",
+		"[color=#1f1f1f]Brioche follows Pita to his bedroom to sleep.[/color]",
+	])
