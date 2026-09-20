346a287bf9b8abbd39f90c973fc41df13db3a687
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-09-20 18:31:11 +0800

    feat: split backyard end dialogue into two parts

--- sidescroll_system/rooms/backyard/phase1/backyard_phase_1.gd
+++ sidescroll_system/rooms/backyard/phase1/backyard_phase_1.gd
@@ -1,14 +1,26 @@
 extends Node2D
 
+var _dialogue_manager: DialogueManager
+var _is_dialogue_finished = false
 
 func _ready() -> void:
-	var dialogue_manager := DialogueManager.get_instance(get_tree())
-	if dialogue_manager == null:
+	_dialogue_manager = DialogueManager.get_instance(get_tree())
+	if _dialogue_manager == null:
 		push_error("No DialogueManager defined for this room.")
 		return
 
-	dialogue_manager.load_messages([
+	_dialogue_manager.load_messages([
 		"[color=#1f1f1f]Bri? Brioche, where'd you go?[/color]",
+	])
+
+
+func _on_dialogue_trigger_area_body_entered(body: Node2D) -> void:
+	print("area entered")
+	print(body.global_position)
+	if _is_dialogue_finished:
+		return
+	
+	_dialogue_manager.load_messages([
 		"[color=#1f1f1f]Oh there you are![/color]",
 		"[color=#1f1f1f]Look at you, digging a hole, are we?[/color]",
 		"[color=#1f1f1f]That's impressive, boy![/color]",
@@ -20,3 +32,5 @@ func _ready() -> void:
 		"[color=#1f1f1f]Brioche growls in annoyance but[/color]",
 		"[color=#1f1f1f]eventually gives in because of food.[/color]",
 	])
+	_is_dialogue_finished = true
+	
