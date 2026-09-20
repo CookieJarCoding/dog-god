381599169f2a07989890ac0dacb73f1f6412f89f
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-09-20 22:11:59 +0800

    feat: create base kitchen structure

---
+++ sidescroll_system/rooms/kitchen/kitchen.gd
@@ -0,0 +1,8 @@
+extends Room
+
+func _ready() -> void:
+	room_phases = [
+		preload("res://sidescroll_system/rooms/kitchen/phase0/kitchen_phase0.tscn"),
+		preload("res://sidescroll_system/rooms/kitchen/phase1/kitchen_phase1.tscn"),	
+	]
+	super()
