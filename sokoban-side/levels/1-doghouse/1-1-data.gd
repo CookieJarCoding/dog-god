d8ea292e8eaa4ad30548ecf8663781ee0816de3c
202516531 <jjcarandang1@up.edu.ph>
2026-09-20 13:59:05 +0800

    Added first two Doghouse levels

---
+++ sokoban-side/levels/1-doghouse/1-1-data.gd
@@ -0,0 +1,22 @@
+extends LevelData
+
+func _enter_tree() -> void:
+	level_name = "In the Doghouse"
+	sigil_queue = [
+		Level.Sigil.A, 
+		Level.Sigil.B,
+	]
+	
+	palette = PaletteList.DOGHOUSE
+	palette_dark = PaletteList.ATTIC_DARK
+	next_level = "res://sokoban-side/levels/1-doghouse/1-2.tscn"
+	
+	Level.lvl_data = self
+	
+func _ready() -> void:
+	PaletteSwapper.set_palette(palette)
+	PaletteSwapper.set_brightness(-1.0)
+	PaletteSwapper.tween_to_brightness(0.0, 0.5)
+
+func _exit_tree() -> void:
+	Level.lvl_data = null
