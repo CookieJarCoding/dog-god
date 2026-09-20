07112d56149a6f80cca5e9fba5e33c1fd538953f
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-09-20 09:33:43 +0800

    chore: change complete button for placeholder sokoban

--- sidescroll_system/rooms/placeholder_eldritchworld.gd
+++ sidescroll_system/rooms/placeholder_eldritchworld.gd
@@ -8,5 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed('A'):
+	if Input.is_action_pressed('B'):
 		RoomLoader.finish_sokoban()
