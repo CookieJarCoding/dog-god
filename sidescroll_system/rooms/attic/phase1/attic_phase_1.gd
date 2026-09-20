b2769b54ea257f32173565c178d16d2486e54f27
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-09-21 01:47:08 +0800

    feat: complete attic phase 1

---
+++ sidescroll_system/rooms/attic/phase1/attic_phase_1.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var darkness: ColorRect = $DarknessLayer
+
+var _dialogue_manager: DialogueManager
+
+@export var sokoban_scene: PackedScene
+
+func _ready() -> void:
+	_dialogue_manager = DialogueManager.get_instance(get_tree())
+	if _dialogue_manager == null:
+		push_error("No DialogueManager defined for this room.")
+		return
+
+	_dialogue_manager.load_messages([
+		"[color=#1f1f1f]Brioche accidentally knocks over a few[/color]",
+		"[color=#1f1f1f]books, making a loud noise.[/color]",
+		"[color=#1f1f1f]She freezes as she hears some[/color]",
+		"[color=#1f1f1f]shuffling downstairs.[/color]",
+		"[color=#1f1f1f](Curses. I've been caught.)[/color]",
+		"[color=#1f1f1f]Hello? Who goes there...[/color]",
+		"[color=#1f1f1f]Brioche stays still, until she hears Pita go down[/color]",
+		"[color=#1f1f1f]the stairs.[/color]",
+		"[color=#1f1f1f](I must continue.)[/color]",
+	])
+	await _dialogue_manager.finished
+	RoomLoader.start_sokoban(sokoban_scene)
