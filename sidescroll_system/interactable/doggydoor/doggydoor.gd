e308bcae259df71408b07ce72b02770b99b5ac9c
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-09-21 13:15:00 +0800

    feat: add transition when entering new room

--- sidescroll_system/interactable/doggydoor/doggydoor.gd
+++ sidescroll_system/interactable/doggydoor/doggydoor.gd
@@ -11,4 +11,5 @@ func interact() -> void:
 	if next_room == null:
 		push_error("No next room defined for this doggy door")
 		return
+	await PaletteSwapper.tween_to_brightness(0.3, 2)
 	RoomLoader.enter_room(next_room)
