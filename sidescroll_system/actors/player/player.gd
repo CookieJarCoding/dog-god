263d0b47d27554d7249dca05f955636fc778804d
serenesplash <cosmicmysticq@gmail.com>
2026-09-21 11:18:08 +0800

    feat: sidescroller sprites (#9)
    * brioche and peter
    
    * simple breathing idle

--- sidescroll_system/actors/player/player.gd
+++ sidescroll_system/actors/player/player.gd
@@ -4,17 +4,20 @@ extends CharacterBody2D
 @export var speed := 75.0
 
 @onready var _collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D 
 @onready var _bark_indicator: Label = $Bark
 
 var _nearby_objects: Array[Interactable] = []
 var _current_interactable: Interactable = null
+var _is_barking := false
 
 func _physics_process(_delta: float) -> void:
+	_handle_animation()
 	velocity = Vector2.ZERO
 
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right") && not _is_barking:
 		velocity.x += 1
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") && not _is_barking:
 		velocity.x -= 1
 
 	velocity = velocity.normalized() * speed
@@ -51,13 +54,21 @@ func get_horizontal_collision() -> Vector2:
 
 
 func _bark() -> void:
-	_bark_indicator.show()
-	await get_tree().create_timer(0.25).timeout
-	_bark_indicator.hide()
+	_is_barking = true
+	await get_tree().create_timer(0.20).timeout
+	_is_barking = false
 
 
 func _face_direction(direction: Vector2) -> void:
 	if direction.x > 0:
-		$Sprite2D.flip_h = true
+		_sprite.flip_h = true
 	elif direction.x < 0:
-		$Sprite2D.flip_h = false
+		_sprite.flip_h = false
+
+func _handle_animation() -> void:
+	if _is_barking:
+		_sprite.play("bark")
+	elif velocity.x != 0:
+		_sprite.play("walk")
+	else:
+		_sprite.play("idle")
